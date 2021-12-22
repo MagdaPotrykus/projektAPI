8dc875070d1eda58a254e5349e13757272335b69
MagdaPotrykus <magda.potrykus@hotmail.com>
2021-12-22 20:09:07 +0100

    Aktualizacja struktury

---
+++ src/Entity/Service.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ServiceRepository::class)
+ */
+class Service
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $serviceName;
+
+    public function __toString()
+    {
+        return $this->getServiceName();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getServiceName(): ?string
+    {
+        return $this->serviceName;
+    }
+
+    public function setServiceName(string $serviceName): self
+    {
+        $this->serviceName = $serviceName;
+
+        return $this;
+    }
+}
